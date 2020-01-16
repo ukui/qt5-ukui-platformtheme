40005cb25571687cfa99e773eba2a393185a264a
Yue-Lan <lanyue@kylinos.cn>
2020-01-16 17:28:35 +0800

    limit the transparent widget to blur.

--- ukui-styles/proxy-style/proxy-style.cpp
+++ ukui-styles/proxy-style/proxy-style.cpp
@@ -33,8 +33,20 @@ void ProxyStyle::polish(QWidget *widget)
 
     //qDebug()<<"\n\n\n============widget mask"<<widget->metaObject()->className()<<widget->mask();
 
-    if (widget->testAttribute(Qt::WA_TranslucentBackground))
-        m_blur_helper->registerWidget(widget);
+    /*!
+      \todo
+      register transparent widget to blurhelper with better way.
+      for now it will let some transparent widget show in error.
+      i have to avoid them by limitting widget's class name,
+      but that is no my expected.
+      */
+    if (widget->testAttribute(Qt::WA_TranslucentBackground)) {
+        if (QString(widget->metaObject()->className())=="QMenu" ||
+                widget->inherits("Peony::DirectoryViewMenu") ||
+                widget->inherits("Peony::DesktopMenu")) {
+            m_blur_helper->registerWidget(widget);
+        }
+    }
 
     //qDebug()<<widget->metaObject()->className();
     //add exception.
