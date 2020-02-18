aa38a3476a9bf03b8f137a70df6a6a9103b35e01
Yue-Lan <lanyue@ubuntukylin.com>
2020-02-18 15:04:29 +0800

    ApplicationStyleSettings: provide read/write application color and palette's api.

--- libqt5-ukui-style/settings/application-style-settings.cpp
+++ libqt5-ukui-style/settings/application-style-settings.cpp
@@ -1,8 +1,32 @@
+/*
+ * Qt5-UKUI's Library
+ *
+ * Copyright (C) 2020, Tianjin KYLIN Information Technology Co., Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: Yue Lan <lanyue@kylinos.cn>
+ *
+ */
+
 #include "application-style-settings.h"
 #include <QApplication>
 #include <QtConcurrent/QtConcurrent>
 #include <QStandardPaths>
 #include <QStyle>
+
+#include <QFileSystemWatcher>
 
 static ApplicationStyleSettings *global_instance = nullptr;
 
@@ -18,6 +42,31 @@ const QString ApplicationStyleSettings::currentCustomStyleName()
     if (m_style_stretagy == Default)
         return nullptr;
     return m_current_custom_style_name;
+}
+
+void ApplicationStyleSettings::setColor(const QPalette::ColorRole &role, const QColor &color, const QPalette::ColorGroup &group)
+{
+    beginGroup(m_color_group_enum.key(group));
+    setValue(m_color_role_enum.key(role), color);
+    endGroup();
+    QtConcurrent::run([=](){
+        this->sync();
+    });
+    auto palette = QApplication::palette();
+    palette.setColor(group, role, color);
+    QApplication::setPalette(palette);
+    qApp->paletteChanged(palette);
+}
+
+const QColor ApplicationStyleSettings::getColor(const QPalette::ColorRole &role, const QPalette::ColorGroup &group)
+{
+    beginGroup(m_color_role_enum.key(group));
+    auto data = value(m_color_role_enum.key(role));
+    QColor color = qvariant_cast<QColor>(data);
+    endGroup();
+    if (color.isValid())
+        return color;
+    return QApplication::palette().color(group, role);
 }
 
 void ApplicationStyleSettings::setColorStretagy(ApplicationStyleSettings::ColorStretagy stretagy)
@@ -50,6 +99,52 @@ void ApplicationStyleSettings::setCustomStyle(const QString &style)
     QApplication::setStyle(style);
 }
 
+void ApplicationStyleSettings::refreshData(bool forceSync)
+{
+    sync();
+    m_custom_palette = QApplication::palette();
+
+    auto color_stretagy = qvariant_cast<ColorStretagy>(value("color-stretagy"));
+    if (color_stretagy != m_color_stretagy) {
+        m_color_stretagy = color_stretagy;
+        Q_EMIT colorStretageChanged(m_color_stretagy);
+    }
+
+    auto style_stretagy = qvariant_cast<StyleStretagy>(value("style-stretagy"));
+    if (style_stretagy != m_style_stretagy) {
+        m_style_stretagy = style_stretagy;
+        Q_EMIT styleStretageChanged(m_style_stretagy);
+    }
+
+    auto current_custom_style_name = value("custom-style").toString();
+    if (m_current_custom_style_name != current_custom_style_name) {
+        m_current_custom_style_name = current_custom_style_name;
+        QApplication::setStyle(m_current_custom_style_name);
+    }
+
+    readPalleteSettings();
+
+    if (forceSync) {
+        QtConcurrent::run([=](){
+            this->sync();
+        });
+    }
+}
+
+void ApplicationStyleSettings::readPalleteSettings()
+{
+    for (int i = 0; i < m_color_group_enum.keyCount(); i++) {
+        beginGroup(m_color_group_enum.key(i));
+        for (int j = 0; j < m_color_role_enum.keyCount(); j++) {
+            auto data = value(m_color_role_enum.key(j));
+            if (data.isValid()) {
+                m_custom_palette.setColor(QPalette::ColorGroup(i),
+                                          QPalette::ColorRole(j),
+                                          qvariant_cast<QColor>(data));
+            }
+        }
+    }
+}
 
 ApplicationStyleSettings::ApplicationStyleSettings(QObject *parent) : QSettings(parent)
 {
@@ -61,7 +156,18 @@ ApplicationStyleSettings::ApplicationStyleSettings(QObject *parent) : QSettings(
 //    QString configPath = configDir + "/" + configFileName;
 //    setPath(QSettings::IniFormat, QSettings::UserScope, configPath);
 //    setDefaultFormat(QSettings::IniFormat);
-    m_color_stretagy = ColorStretagy(value("color-stretagy").toInt());
-    m_style_stretagy = StyleStretagy(value("style-stretagy").toInt());
+    setDefaultFormat(QSettings::IniFormat);
+    setAtomicSyncRequired(true);
+
+    m_color_stretagy = qvariant_cast<ColorStretagy>(value("color-stretagy"));
+    m_style_stretagy = qvariant_cast<StyleStretagy>(value("style-stretagy"));
     m_current_custom_style_name = value("custom-style").toString();
+    m_custom_palette = QApplication::palette();
+    readPalleteSettings();
+
+    QFileSystemWatcher *watcher = new QFileSystemWatcher(this);
+    watcher->addPath(fileName());
+    connect(watcher, &QFileSystemWatcher::fileChanged, [=](){
+        refreshData();
+    });
 }
--- libqt5-ukui-style/settings/application-style-settings.h
+++ libqt5-ukui-style/settings/application-style-settings.h
@@ -1,7 +1,31 @@
+/*
+ * Qt5-UKUI's Library
+ *
+ * Copyright (C) 2020, Tianjin KYLIN Information Technology Co., Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: Yue Lan <lanyue@kylinos.cn>
+ *
+ */
+
 #ifndef APPLICATIONSTYLESETTINGS_H
 #define APPLICATIONSTYLESETTINGS_H
 
 #include <QSettings>
+#include <QPalette>
+#include <QMetaEnum>
 
 class QStyle;
 
@@ -11,6 +35,9 @@ class QStyle;
  * This class is used to decide the style switch stretagies for independent application.
  * For example, you can choose the color scheme switch stretagy of an application, hold
  * the color in light or dark, or follow the system's palette.
+ *
+ * \note
+ * This API is unstable, if possible, do not use it.
  */
 class ApplicationStyleSettings : public QSettings
 {
@@ -36,6 +63,11 @@ public:
     StyleStretagy currentStyleStretagy() {return m_style_stretagy;}
     const QString currentCustomStyleName();
 
+    void setColor(const QPalette::ColorRole &role, const QColor &color, const QPalette::ColorGroup &group = QPalette::Active);
+    const QColor getColor(const QPalette::ColorRole &role, const QPalette::ColorGroup &group = QPalette::Active);
+
+    QPalette getCustomPalette() {return m_custom_palette;}
+
 signals:
     void colorStretageChanged(const ColorStretagy &stretagy);
     void styleStretageChanged(const StyleStretagy &stretagy);
@@ -45,6 +77,10 @@ public slots:
     void setStyleStretagy(StyleStretagy stretagy);
     void setCustomStyle(const QString &style);
 
+protected:
+    void refreshData(bool forceSync = false);
+    void readPalleteSettings();
+
 private:
     explicit ApplicationStyleSettings(QObject *parent = nullptr);
     ~ApplicationStyleSettings() {}
@@ -52,6 +88,10 @@ private:
     ColorStretagy m_color_stretagy;
     StyleStretagy m_style_stretagy;
     QString m_current_custom_style_name;
+    QMetaEnum m_color_role_enum = QMetaEnum::fromType<QPalette::ColorRole>();
+    QMetaEnum m_color_group_enum = QMetaEnum::fromType<QPalette::ColorGroup>();
+
+    QPalette m_custom_palette;
 };
 
 #endif // APPLICATIONSTYLESETTINGS_H
