d6fd4bd5cfa64a7fc98b9a449c9c299c511010ae
lanyue <lanyue@kylinos.cn>
2020-06-12 14:21:53 +0800

    WindowManager: refactor window moving handler. GestureHelper: fix the crash after program about to quit.

--- ukui-styles/proxy-style/window-manager.cpp
+++ ukui-styles/proxy-style/window-manager.cpp
@@ -25,6 +25,7 @@
 #include <QWidget>
 #include <QMouseEvent>
 
+#include <KWindowSystem>
 #include <NETWM>
 #include <QX11Info>
 #include <xcb/xcb.h>
@@ -76,19 +77,10 @@ bool WindowManager::eventFilter(QObject *obj, QEvent *e)
         return false;
     }
     case QEvent::MouseMove: {
-        if (m_is_dragging) {
-            if (m_current_obj != obj)
-                return false;
-            if (m_timer.isActive()) {
-                return false;
-            } else {
-                if (QWidget::mouseGrabber()) return false;
-                QMouseEvent *event = static_cast<QMouseEvent*>(e);
-                //move request
-                mouseMoveEvent(obj, event);
-                return true;
-            }
-        }
+        if (QWidget::mouseGrabber()) return false;
+        QMouseEvent *event = static_cast<QMouseEvent*>(e);
+        //move request
+        mouseMoveEvent(obj, event);
         return false;
     }
     case QEvent::MouseButtonRelease: {
@@ -122,7 +114,8 @@ void WindowManager::buttonPresseEvent(QObject *obj, QMouseEvent *e)
         return;
     }
 
-    m_is_dragging = true;
+    m_prepared_to_drag = true;
+    m_is_dragging = false;
     m_current_obj = obj;
     m_start_point = e->pos();
     m_timer.start();
@@ -130,23 +123,43 @@ void WindowManager::buttonPresseEvent(QObject *obj, QMouseEvent *e)
 
 void WindowManager::mouseMoveEvent(QObject *obj, QMouseEvent *e)
 {
-    if (!m_is_dragging)
+    //qDebug()<<"mouse move event";
+
+    if (!m_prepared_to_drag)
         return;
-    //qDebug()<<"move";
+
     QWidget *w = qobject_cast<QWidget*>(obj);
-
     const QPoint native = e->globalPos();
+    if (e->source() != Qt::MouseEventSynthesizedByQt && KWindowSystem::isPlatformX11()) {
+        if (m_is_dragging)
+            return;
 
-    auto connection = QX11Info::connection();
-    xcb_ungrab_pointer(connection, XCB_TIME_CURRENT_TIME);
-    NETRootInfo(connection, NET::WMMoveResize).moveResizeRequest(w->winId(), native.x(), native.y(), NET::Move);
+        qDebug()<<"x11 move start";
+        auto connection = QX11Info::connection();
+        xcb_ungrab_pointer(connection, XCB_TIME_CURRENT_TIME);
+        NETRootInfo(connection, NET::WMMoveResize).moveResizeRequest(w->winId(), native.x(), native.y(), NET::Move);
+        qDebug()<<"x11 move end";
 
-    m_start_point = e->pos();
-    m_timer.start();
+        QMouseEvent me(QMouseEvent::MouseButtonRelease, e->localPos(), e->button(), e->buttons(), e->modifiers());
+        qApp->sendEvent(obj, &me);
+
+        m_is_dragging = true;
+
+        m_timer.start();
+    } else {
+        auto widget = qobject_cast<QWidget *>(obj);
+        auto topLevel = widget->topLevelWidget();
+        auto globalPos = QCursor::pos();
+        //auto offset = globalPos - m_press_pos;
+        topLevel->move(globalPos - topLevel->mapFrom(widget, m_start_point));
+    }
+
+    return;
 }
 
 void WindowManager::mouseReleaseEvent(QObject *obj, QMouseEvent *e)
 {
+    //qDebug()<<"mouse release event";
     endDrag();
 }
 
@@ -167,6 +180,7 @@ AppEventFilter::AppEventFilter(WindowManager *parent) : QObject(parent)
 bool AppEventFilter::eventFilter(QObject *obj, QEvent *e)
 {
     if (e->type() == QEvent::MouseButtonRelease) {
+        m_wm->m_prepared_to_drag = false;
         m_wm->m_is_dragging = false;
         m_wm->m_current_obj = nullptr;
         m_wm->m_timer.stop();
